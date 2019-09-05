a6c976e27990c001c8278345e787270b9b4eaaa8
sriz0006 <sriz0006@student.monash.edu>
2019-09-05 08:48:53 +0200

    commit initial

--- app.js
+++ app.js
@@ -1,21 +1,23 @@
-var express = require('express');
-var indexRouter = require('./routes/index.js');
-var bodyParser = require('body-parser');
-var path = require('path');
-var app = express();
+/* eslint-disable no-undef */
+var express = require('express')
+// eslint-disable-next-line no-undef
+var indexRouter = require('./routes/index.js')
+var bodyParser = require('body-parser')
+var path = require('path')
+var app = express()
 
-app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'html');
+app.engine('html', require('ejs').renderFile)
+app.set('view engine', 'html')
 
-app.use(express.static(path.join(__dirname, 'public')));
-//app.use(express.static('bower_components'));
-app.use('/bootstrap', express.static(path.join(__dirname, 'bower_components/bootstrap/')));
-app.use('/jquery', express.static(path.join(__dirname, 'bower_components/jquery/')));
-app.use('/css', express.static(path.join(__dirname, 'public/stylesheets/')));
+app.use(express.static(path.join(__dirname, 'public')))
+// app.use(express.static('bower_components'));
+app.use('/bootstrap', express.static(path.join(__dirname, 'bower_components/bootstrap/')))
+app.use('/jquery', express.static(path.join(__dirname, 'bower_components/jquery/')))
+app.use('/css', express.static(path.join(__dirname, 'public/stylesheets/')))
 app.use(bodyParser.urlencoded({
-    extended: false
-}));
+  extended: false
+}))
 
-app.use('/', indexRouter);
-app.listen("8080");
-console.log("Server running at http://localhost:8080");
+app.use('/', indexRouter)
+app.listen('8080')
+console.log('Server running at http://localhost:8080')
